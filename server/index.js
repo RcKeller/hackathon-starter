ca35213c9d1ce801395cbdaa20c98a596a709ebd
Ryan Keller <rykeller@uw.edu>
2019-05-16 18:03:57 -0600

    Reorganized

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
 const isDev = process.env.NODE_ENV !== 'production';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
