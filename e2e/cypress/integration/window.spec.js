f7ce15e1090edc33af8c29e0a303682eae1e9a66
Ryan Keller <rykeller@uw.edu>
2019-05-16 18:09:07 -0600

    Fixed integration tests

--- e2e/cypress/integration/window.spec.js
+++ e2e/cypress/integration/window.spec.js
@@ -2,21 +2,28 @@
 
 context('Window', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/commands/window')
+    cy.visit('/')
   })
 
   it('cy.window() - get the global window object', () => {
     // https://on.cypress.io/window
-    cy.window().should('have.property', 'top')
+    cy
+      .window()
+      .should('have.property', 'top')
   })
 
   it('cy.document() - get the document object', () => {
     // https://on.cypress.io/document
-    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    cy
+      .document()
+      .should('have.property', 'charset')
+      .and('eq', 'UTF-8')
   })
 
   it('cy.title() - get the title', () => {
     // https://on.cypress.io/title
-    cy.title().should('include', 'Kitchen Sink')
+    cy
+      .title()
+      .should('exist')
   })
 })
