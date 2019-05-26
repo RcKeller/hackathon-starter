439b38732c33c992b01c5163a94775ce587cef84
Ryan Keller <rykeller@uw.edu>
2019-05-25 22:49:36 -0600

    Got clustering to work

--- server/src/lib/clustering.js
+++ server/src/lib/clustering.js
@@ -1,14 +1,15 @@
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-
-console.log(`[Clustering]: Initializing clustering, leveraging ${numCPUs} CPUs`)
+// MUST BE DEFINED IN CLOSURE SCOPE!
+const isDev = process.env.NODE_ENV !== 'production';
 
 /*
 CLUSTERING - initializes clustering, then runs the app as a callback.
 */
+console.log(`[Clustering]: Initializing clustering, leveraging ${numCPUs} CPUs`)
 module.exports = (callback) => {
   // Multi-process to utilize all CPU cores.
-  if (!process.env.NODE_ENV !== 'production' && cluster.isMaster) {
+  if (!isDev && cluster.isMaster) {
     console.error(`[${process.pid} - Clustering]: master is running`);
 
     // Fork workers.
@@ -24,46 +25,3 @@ module.exports = (callback) => {
     callback()
   }
 }
-
-// const express = require('express');
-// const path = require('path');
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
-
-// const isDev = process.env.NODE_ENV !== 'production';
-// const PORT = process.env.PORT || 8080;
-
-// // Multi-process to utilize all CPU cores.
-// if (!isDev && cluster.isMaster) {
-//   console.error(`Node cluster master ${process.pid} is running`);
-
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
-
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-//   });
-
-// } else {
-//   const app = express();
-
-//   // Priority serve any static files.
-//   app.use(express.static(path.resolve(__dirname, '../../client/build')));
-
-//   // Answer API requests.
-//   app.get('/api', function (req, res) {
-//     res.set('Content-Type', 'application/json');
-//     res.send('{"message":"Hello from the custom server!"}');
-//   });
-
-//   // All remaining requests return the React app, so it can handle routing.
-//   app.get('*', function (request, response) {
-//     response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-//   });
-
-//   app.listen(PORT, function () {
-//     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-//   });
-// }
--- server/src/server.js
+++ server/src/server.js
@@ -1,42 +1,23 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 
-const isDev = process.env.NODE_ENV !== 'production';
-const PORT = process.env.PORT || 8080;
+const express = require('express')
+const { Express, HTTP } = require('./setup')
+const { API, Static } = require('./routes')
+const config = require('../config')
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const Clustering = require('./lib/clustering');
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// Core process exception handling
+require('./lib/exceptions');
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
-
-} else {
-  const app = express();
-
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../../client/build')));
-
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
-
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-  });
-}
+Clustering(() => {
+  // Initialize express instance and configure parsers / sessionware
+  const server = express()
+  Express(server, config)
+  
+  // Initialize routes - API, client pages, etc
+  Static(server, config)
+  API(server, config)
+  
+  // Serve content via HTTP or HTTPS
+  HTTP(server, config)
+})
